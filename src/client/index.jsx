dc1a42301d1941e054dbd904e301b197a4085c80
Aleksandr <d100@me.com>
2020-05-31 17:54:58 +0300

    replace app component to a new file

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-export default () => {
-  return (
-    <div>Hello Stream Rendering</div>
-  )
-}
+window.addEventListener('load', () => {
+  ReactDOM.hydrate(<App />, document.getElementById('root'));
+});
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import ReactDOM from 'react-dom/server';
 import React from 'react';
-import App from '../client';
+import App from '../client/App';
 
 const app = express();
 const rootComponent = React.createElement(App);
+
+app.use('/static', express.static('./dist'));
 
 app.get('/', (req, res) => {
   res.send(ReactDOM.renderToString(rootComponent))
